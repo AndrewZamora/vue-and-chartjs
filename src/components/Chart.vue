fdaf3f0e6dd46b1882a37fac3aa41906bb85bd41
Andrew Zamora <andrewmichaelzamora@gmail.com>
2020-06-24 23:22:31 +0900

    Initial Commit

---
+++ src/components/Chart.vue
@@ -0,0 +1,34 @@
+<template>
+  <canvas ref="chart"></canvas>
+</template>
+
+<script>
+import Chart from "chart.js";
+export default {
+  data() {
+    return {
+      chart: null
+    };
+  },
+  props: ["data"],
+  mounted() {
+    this.chart = new Chart(this.$refs.chart, this.data);
+  },
+  computed: {
+    dataSets() {
+      return this.data.datasets;
+    }
+  },
+  watch: {
+    dataSets(newDatasets, oldDatasets) {
+      if (newDatasets !== oldDatasets) {
+        this.chart.data.datasets = newDatasets;
+        this.chart.update();
+      }
+    }
+  }
+};
+</script>
+
+<style>
+</style>
